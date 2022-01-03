02cb361c5f01978381fb06140a866d52a9525fcd
jeilynng <97058855+jeilynng@users.noreply.github.com>
2022-01-03 15:10:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
 
-// App set up
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile("Public/index.html", {root: __dirname});
-})
-app.use(express.static('Public'));
+let db = new sqlite3.Database(':memory:');
 
-// Listen
-app.listen(3000, () => console.log('started'));
+db.serialize(function () {
+  db.run('CREATE TABLE images(name TEXT, desc TEXT)');
+  var stmt = db.prepare('INSERT INTO images values(?, ?)');
+  stmt.run('Image 1', 'Impressive work!');
+  stmt.run('Image 2', 'Wow!');
+  stmt.finalize();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('public/index.html');
+});
+
+app.get('/images', (req, res) => {
+  db.all('SELECT name, desc FROM images', (err, rows) => {
+    res.send(rows);
+  });
+})
+
+app.listen(3000, () => console.log('server started'));
